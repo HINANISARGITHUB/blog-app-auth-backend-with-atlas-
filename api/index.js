8e1ffbec1715f0bf3afa21e0f4f5fbe8985b65b7
HINANISARGITHUB <hinanisar7860@gmail.com>
2026-05-05 19:58:37 +0500

    first commit

--- api/index.js
+++ api/index.js
@@ -2,25 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import connectDB from './config/db.js';
-import authRoutes from './routes/auth.js';
+import connectDB from '../config/db.js';
+import authRoutes from '../routes/auth.js';
 
 const app = express();
 
 // Database Connection
 connectDB();
 
-// Updated CORS Configuration
-// app.use(cors({
- 
-//   origin: [
-//     process.env.FRONTEND_URL, 
-    
-//   ],
-//   credentials: true,
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Authorization"]
-// }));
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true,
@@ -38,6 +27,6 @@ app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => res.send('API is running...'));
 
 const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
