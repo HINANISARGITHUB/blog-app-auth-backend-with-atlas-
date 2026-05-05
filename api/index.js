87e70b6864db40d264dbb8e08270af57b8bda5ba
Hina Nisar <160288150+HINANISARGITHUB@users.noreply.github.com>
2026-05-05 21:05:12 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use('/api/auth', authRoutes);
 // Root route for Vercel health check
 app.get('/', (req, res) => res.send('API is running...'));
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 export default app;
