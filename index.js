710d62a7d5666553ad439fd8641f48ab4213b0d2
HINANISARGITHUB <hinanisar7860@gmail.com>
2026-05-05 10:22:45 +0500

    env changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
   // Multiple origins allowed (ENV variable + Hardcoded for safety)
   origin: [
     process.env.FRONTEND_URL, 
-    'https://blog-app-auth-backend.vercel.app'
+    
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
