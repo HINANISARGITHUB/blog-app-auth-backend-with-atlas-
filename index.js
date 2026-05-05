cee8bcb5bce679d00b2e5dd5ad4a8d98e406a59e
HINANISARGITHUB <hinanisar7860@gmail.com>
2026-05-05 10:56:29 +0500

    index.js

--- index.js
+++ index.js
@@ -11,14 +11,20 @@ const app = express();
 connectDB();
 
 // Updated CORS Configuration
-app.use(cors({
-  // Multiple origins allowed (ENV variable + Hardcoded for safety)
-  origin: [
-    process.env.FRONTEND_URL, 
+// app.use(cors({
+ 
+//   origin: [
+//     process.env.FRONTEND_URL, 
     
-  ],
+//   ],
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type", "Authorization"]
+// }));
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
@@ -32,4 +38,6 @@ app.use('/api/auth', authRoutes);
 app.get('/', (req, res) => res.send('API is running...'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
