68cf946fd4f2fb66f8fa711499b8faaccdda0718
HINANISARGITHUB <hinanisar7860@gmail.com>
2026-05-06 05:15:59 +0500

    env

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors({
   // Multiple origins allowed (ENV variable + Hardcoded for safety)
   origin: [
     process.env.FRONTEND_URL, 
-    // 'https://blog-app-auth-frontend-with-atlas.vercel.app'
+     ''
   ],
 
   credentials: true,
