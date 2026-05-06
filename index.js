0c6bef393b365c35d72508ea70b877c7377ecc4f
HINANISARGITHUB <hinanisar7860@gmail.com>
2026-05-06 05:19:43 +0500

    index.js

--- index.js
+++ index.js
@@ -12,10 +12,18 @@ connectDB();
 
 // Universal CORS for Stability
 app.use(cors({
+
   origin: function (origin, callback) {
     // Sab origins allow kar diye taake deployment mein masla na aaye
     callback(null, true);
   },
+
+  // Multiple origins allowed (ENV variable + Hardcoded for safety)
+  origin: [
+    process.env.FRONTEND_URL, 
+    // 'https://blog-app-auth-frontend-with-atlas.vercel.app'
+  ],
+
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"]
